0fcfd54ad350122f1a54c7574520124a63208e6a
AviParampampam <aviparampampam@gmail.com>
2021-05-15 20:48:45 +0500

    add auto-remove files system

--- catalog/models.py
+++ catalog/models.py
@@ -1,10 +1,22 @@
 from django.db import models
+from django.dispatch.dispatcher import receiver
+import os
 
 class Snapshot(models.Model):
     file = models.ImageField(upload_to='snapshots', default='default.jpg')
 
+@receiver(models.signals.pre_delete, sender=Snapshot)
+def auto_delete_snapshot(sender, instance, **kwargs):
+    if instance.file:
+        instance.file.delete()
+
 class VideoFile(models.Model):
     file = models.FileField(upload_to='videos', default='default.mp4')
+
+@receiver(models.signals.pre_delete, sender=VideoFile)
+def auto_delete_videofile(sender, instance, **kwargs):
+    if instance.file:
+        instance.file.delete()
 
 def get_default_videofile():
     return VideoFile.objects.create(file='default.mp4')
@@ -16,3 +28,10 @@ class Video(models.Model):
 
     def __str__(self):
         return self.name
+
+@receiver(models.signals.pre_delete, sender=Video)
+def auto_delete_video(sender, instance, **kwargs):
+    if instance.videofile.file:
+        instance.videofile.file.delete()
+    if instance.snapshot.file:
+        instance.snapshot.file.delete()
